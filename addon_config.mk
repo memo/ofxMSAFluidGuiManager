33bcfc746002170a27f577a30184d8b2cfb269ae
Memo Akten <memo@memo.tv>
2019-02-01 04:51:19 +0300

    addon_config

---
+++ addon_config.mk
@@ -0,0 +1,11 @@
+meta:
+	ADDON_NAME = ofxMSAFluidManager
+	ADDON_DESCRIPTION =
+	ADDON_AUTHOR = Memo Akten, www.memo.tv
+	ADDON_TAGS = "Fluid"
+	ADDON_URL = https://github.com/memo/ofxMSAFluidManager
+
+common:
+	# dependencies with other addons, a list of them separated by spaces
+	# or use += in several lines
+	ADDON_DEPENDENCIES = ofxMSAFluid ofxSimpleGuiToo
